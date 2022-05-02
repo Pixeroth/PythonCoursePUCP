4dbcd31dbf86ec2bb4627e744341e1859e797863
cesarg <cesarg@wolfram.com>
2022-05-02 17:34:58 -0500

    cleaned matplotlib.ipynb and upload new modules.ipynb

---
+++ 6-user-modules/moduleA.py
@@ -0,0 +1,22 @@
+'''
+moduleA documentation
+* Purpoose
+    - example module
+'''
+
+# create a variable
+var = 42
+
+# create functions
+def foo():
+    print("function foo")
+
+def main():
+    print(var)
+    foo()
+    print("main function")
+
+if __name__ == "__main__":
+    print(main())
+    
+    
